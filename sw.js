9ac94ce8e2bc433af0b97efddd33e140dd0c7314
flaviocunhag <flaviocunhag@icloud.com>
2026-09-18 14:24:01 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
-const C='dashtrack-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C = 'dashtrack-v3';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(C)
+      .then(cache => cache.addAll([
+        './',
+        './index.html',
+        './manifest.webmanifest'
+      ]))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== C)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
